344811efb9dde280827a69d3d451cff9b98ec45e
SHADIL AM <109820522+shadil-rayyan@users.noreply.github.com>
2025-03-20 12:20:30 +0530

    Create ghostty.sh

---
+++ linux/ghostty.sh
@@ -0,0 +1,3 @@
+wget https://github.com/mkasberg/ghostty-ubuntu/releases/download/1.1.2-0-ppa1/ghostty_1.1.2-0.ppa1_amd64_22.04.deb
+sudo dpkg -i ghostty_1.1.2-0.ppa1_amd64_2
+rm -rf ghostty_1.1.2-0.ppa1_amd64_2
